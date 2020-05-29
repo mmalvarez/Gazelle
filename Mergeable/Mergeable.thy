297f99b4b9a70e9669810d9243590245fa6b9379
mario.alvarez@consensys.net <yorix@DESKTOP-84OVJN9.localdomain>
2020-05-29 11:53:39 -0700

    some more work on defining merging semantics

--- Mergeable/Mergeable.thy
+++ Mergeable/Mergeable.thy
@@ -497,7 +497,13 @@ has_sup a b \<Longrightarrow> (bsup (bsup a b)) c \<le> bsup a (bsup b c)
 
 *)
 
-lemma sup_assoc_fact1 :
+lemma sup_assoc1 :
+  fixes a b c
+  assumes Hsup : "LLl.has_sup {a, b}"
+  shows "bsup (bsup a b) c = bsup (bsup b a) c"
+  using bsup_comm[OF Hsup] by auto
+
+lemma sup_assoc_lb1 :
   fixes a b c
   assumes Hsup : "LLl.has_sup {a, b}"
   shows "l_pleq (bsup (bsup a b) c) (bsup a (bsup b c))"
@@ -517,13 +523,7 @@ proof(-)
   show ?thesis using 0 1 3 4 by auto
 qed
 
-lemma sup_assoc_fact2 :
-  fixes a b c
-  assumes Hsup : "LLl.has_sup {a, b}"
-  shows "bsup (bsup a b) c = bsup (bsup b a) c"
-  using bsup_comm[OF Hsup] by auto
-
-lemma sup_assoc_fact3 :
+lemma sup_assoc_lb2 :
   fixes a b c
   assumes Hsup : "LLl.has_sup {a, b}"
   shows "l_pleq (bsup (bsup a b) c) (bsup b (bsup a c))"
@@ -544,43 +544,6 @@ proof(-)
 
 qed
 
-(*
-
-lemma assoc_fact3 :
-  "\<And> a b c . bsup (bsup a b) c = (bsup (bsup a b) (bsup a c))"
-proof(-)
-  fix a b c
-  have LtoR : "l_pleq (bsup (bsup a b) c) (bsup (bsup a b) (bsup a c))" using assoc_fact2 by auto
-
-  have RtoL : "l_pleq (bsup (bsup a b) (bsup a c)) (bsup (bsup a b) c)"
-  proof(-)
-    have 0 : "l_pleq (bsup (bsup a b) (bsup a c)) (bsup (bsup (bsup a b) a) (bsup (bsup a b) c))"
-      using assoc_fact2[of a b "bsup a c"]
-
-(*
-  have 4 : "(\<And>bd sd. pleq bd (aug (bsup a c)) \<Longrightarrow> is_sup {aug (bsup a b), bd} sd \<Longrightarrow> pleq bd (aug c))"
-  proof(-)
-    fix bd sd
-    assume Hpleq : "pleq bd (aug (bsup a c))"
-    assume Hsup : "is_sup {aug (bsup a b), bd} sd"
-*)
-  
-  
-(*
-lemma assoc :
-(*  "bsup a (bsup b c) = bsup (bsup a b) c" *)
-"l_pleq (bsup (bsup a b) c) (bsup a (bsup b c))"
-proof(-)
-
-  have 0:  "l_pleq a (bsup a b)" using bsup_spec 
-    by(simp add:aug_merge_bsup_leq)
-
-  have 1 : "l_pleq (bsup a b) (bsup a b)" 
-    using LLl.leq_refl by auto
-
-  have 2 :
-"(\<And>cd sd. pleq cd (aug c) \<Longrightarrow> is_sup {aug (bsup a b), cd} sd \<Longrightarrow> pleq cd (aug (bsup b c)))"
-*)
 
 end
 
@@ -662,176 +625,4 @@ apply(simp add:Aug_Pord.l_pleq_def)
 apply(simp add:Aug_Pord.l_pleq_def)
   done
 
-
-type_synonym t1 = "nat option * nat"
-
-type_synonym t1a = "nat option * nat option"
-
-(*
-definition test_lleq :: "t1 \<Rightarrow> t1 \<Rightarrow> bool" where 
-"test_lleq l r =    (case (l, r) of
-         ((None, l2), (None, r2)) \<Rightarrow> l2 = r2
-       | ((None, l2), (Some r1, r2)) \<Rightarrow> l2 = r2
-       | ((Some _, _), (None, _)) \<Rightarrow> False
-       | ((Some l1, l2 ), (Some r1, r2)) \<Rightarrow> l1 = r1 \<and> l2 = r2)"
-*)
-
-definition test_lleq :: "t1a \<Rightarrow> t1a \<Rightarrow> bool" where 
-"test_lleq l r = 
-   (case (l, r) of
-         ((None, None), (_, _)) \<Rightarrow> True
-         | ((None, Some x2), (_, Some y2)) \<Rightarrow> x2 = y2
-         | ((Some x1, None), (Some y1, _)) \<Rightarrow> x1 = y1
-         | ((Some x1, Some x2), (Some y1, Some y2)) \<Rightarrow> x1 = y1 \<and> x2 = y2
-         | (_, _) \<Rightarrow> False)"
-
-definition test_bsup :: "t1 \<Rightarrow> t1 \<Rightarrow> t1" where
-"test_bsup = (\<lambda> l r .
-    (case (l, r) of
-            ((None, l2), (None, r2)) \<Rightarrow> (None, l2)
-            |((None, l2), (Some r1, r2)) \<Rightarrow> (Some r1, l2)
-            |((Some l1, l2), (None, r2)) \<Rightarrow> (Some l1, l2)
-            |((Some l1, l2), (Some r1, r2)) \<Rightarrow> (Some l1, l2)))"
-
-definition test_aug :: "t1 \<Rightarrow> t1a" where
-"test_aug t =
-  (case t of
-    (x1, x2) \<Rightarrow> (x1, Some x2))"
-
-definition test_deaug :: "t1a \<Rightarrow> t1 option" where
-"test_deaug ta =
-  (case ta of
-    (x1, Some x2) \<Rightarrow> Some (x1, x2)
-    | _ \<Rightarrow> None)"
-
-(*
-print_locale Aug_Mergeable_Spec
-interpretation Test : Aug_Mergeable_Spec test_lleq test_aug test_deaug test_bsup
-
-  apply(unfold_locales)
-              apply(simp add: test_lleq_def split:prod.splits option.splits)
-  apply(simp add: test_lleq_def split:prod.splits option.splits)
-         apply(simp add: test_lleq_def split:prod.splits option.splits)
-
-(* completeness *)
-
-        apply(simp add:Pord.has_ub_def Pord.is_ub_def
-Pord.has_sup_def Pord.is_sup_def Pord.is_least_def)
-
-        apply(clarsimp)
-        apply(simp add:test_lleq_def)
-        apply(simp split:option.splits; (clarsimp; force))
-
-(* end completeness *)
-
-       apply(simp add: test_deaug_def test_aug_def test_lleq_def split:prod.splits option.splits)
-      apply(simp add: test_deaug_def test_aug_def test_lleq_def split:prod.splits option.splits)
-       apply(clarsimp)
-
-      apply(simp add: Aug_Pord.l_pleq_def test_lleq_def test_aug_def test_deaug_def split:prod.splits option.splits)
-      apply(simp add: Aug_Pord.l_pleq_def test_lleq_def test_aug_def test_deaug_def split:prod.splits option.splits)
-     apply(clarsimp) apply(fastforce)
-      apply(simp add: Aug_Pord.l_pleq_def test_lleq_def test_aug_def test_deaug_def split:prod.splits option.splits)
-
-  apply(simp add: Aug_Pord.l_pleq_def Aug_Pord.is_bsup_def Aug_Pord.is_bub_def Pord.is_least_def Pord.is_sup_def Pord.is_ub_def)
-  apply(simp add: test_deaug_def test_aug_def test_lleq_def test_bsup_def)
-
-
-  apply(case_tac a; clarsimp)
-  apply(case_tac aaa; clarsimp)
-   apply(case_tac aa; clarsimp)
-  apply(case_tac a; clarsimp)
-    apply(case_tac b; clarsimp)
-      apply(case_tac bb; clarsimp)
-      apply(case_tac ab; clarsimp) 
-       apply(drule_tac x = None in spec) apply(drule_tac x = "Some a" in spec)
-        apply(clarsimp)
-
-  apply(case_tac bb; clarsimp)
-       apply(case_tac ab; clarsimp) 
-
-       apply(drule_tac x = None in spec) apply(drule_tac x = "Some a" in spec)
-        apply(clarsimp)
-
-  apply(case_tac bb; clarsimp)
-       apply(case_tac ab; clarsimp) 
-    apply(case_tac b; clarsimp)
-
-   apply(rule_tac conjI)
-
-  apply(clarsimp)
-    apply(case_tac ab; clarsimp)
-    apply(case_tac aa; clarsimp)
-
-    apply(case_tac b; clarsimp)
-       apply(case_tac bb; clarsimp)
-      apply(case_tac bb; clarsimp)
-     apply(case_tac bb; clarsimp)
-    apply(case_tac aa; clarsimp)
-     apply(case_tac bb; clarsimp)
-    apply(case_tac b; clarsimp)
-
-       apply(drule_tac x = "None"  in spec) apply(drule_tac x = "Some aa" in spec)
-        apply(clarsimp)
-  
-       apply(drule_tac x = "None"  in spec) apply(drule_tac x = "Some aa" in spec)
-       apply(clarsimp)
-
-      apply(case_tac b; clarsimp)
-     apply(case_tac bb; clarsimp)
-    apply(case_tac bb; clarsimp)
-
-  apply(clarsimp)
-  apply(simp cong:option.case_cong)
-  apply(case_tac aa; clarsimp)
-  apply(simp cong:option.case_cong)
-
-       apply(drule_tac x = "Some a" in spec) apply(clarsimp)
-       apply(drule_tac x = "None" in spec)
-       apply(clarsimp)
-
-       apply(drule_tac x = "Some a" in spec) apply(drule_tac x = "Some baa" in spec)
-      apply(clarsimp)
-    apply(case_tac b; clarsimp) apply(case_tac a; clarsimp)
-     apply(case_tac aa; clarsimp)
-apply(case_tac aa; clarsimp)
-
-       apply(drule_tac x = "Some a" in spec) apply(clarsimp)
-       apply(drule_tac x = "None" in spec)
-   apply(clarsimp)
-
-  apply(simp split:option.splits) apply(clarsimp)
-
-
-  apply(case_tac aa; clarsimp)
-   apply(case_tac ab; clarsimp)
-   apply(case_tac b; clarsimp)
-   apply(case_tac ac; clarsimp)
-     apply(simp  split:option.splits)
-
-   apply(case_tac ac; clarsimp)
-    apply(simp  split:option.splits)
-
-
-   apply(case_tac ac; clarsimp)
-   apply(simp  split:option.splits)
-
-   apply(case_tac b; clarsimp)
-   apply(case_tac ac; clarsimp)
-apply(case_tac ad; clarsimp)
-    apply(simp  split:option.splits)
-apply(case_tac ad; clarsimp)
-  apply(simp  split:option.splits)
-
-   apply(case_tac ac; clarsimp)
-apply(case_tac ad; clarsimp)
-    apply(simp  split:option.splits)
-apply(case_tac ad; clarsimp)
-  apply(simp  split:option.splits)
-
-  done
-(* ok, so this seems to work - but is rather ugly. *)
-
-*)
-*)
 end
