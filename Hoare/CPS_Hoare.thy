c92213dc8b35b4072937327a72e512c4650fe8b6
Mario Alvarez <mario.alvarez@mesh.xyz>
2021-05-13 02:08:02 -0700

    a revised approach to CPS Hoare logic, but still needs some revision

--- Hoare/CPS_Hoare.thy
+++ Hoare/CPS_Hoare.thy
@@ -1,32 +1,44 @@
 theory CPS_Hoare imports 
  "../Gensyn" "../Gensyn_Descend" "../Mergeable/Mergeable"
  "../Lifting/LiftUtils" "../Lifting/LiftInstances" "../Lifting/LangCompSimple"
+ "../Relpath" "../Semantics/Gensyn_Sem_Small"
 begin
 
 (* ok, so in order to adapt CPS hoare triples into this context,
    we need a more "normal-looking" system where we can separate
    continuations from state *)
 
-datatype 'syn s_error =
-  Exec 'syn 
+datatype s_error =
+  Exec childpath
   | BadPath
   | NoFuel
   | Done
+  | Crash
   | Halted (* halted = too much fuel *)
 
-fun s_error_safe :: "'x s_error \<Rightarrow> bool" where
+fun s_error_safe :: "s_error \<Rightarrow> bool" where
 "s_error_safe (Exec _) = True"
 | "s_error_safe Done = True"
 | "s_error_safe _ = False"
 
 (* we also need to be able to separate sub-syntax from entire tree *)
 
+(* childpath representation
+   
+*)
+
 record ('syn, 'mstate) sem' =
   s_sem :: "'syn \<Rightarrow> 'mstate \<Rightarrow> 'mstate"
 
+(* TODO: more granular errors? (e.g. s_error)? *)
 record ('syn, 'mstate) sem = "('syn, 'mstate) sem'" +
-  s_next :: "'mstate \<Rightarrow> ('syn s_error)"
+  s_cont :: "'mstate \<Rightarrow> 'syn gensyn list option"
 
+(* OK, for CPS approach we need to have each control-flow state
+   be able to manipulate and return a list of targets *)
+
+
+(* where is path update *)
 
 (* need to deal with this difference around halting behavior. *)
 (*
@@ -46,31 +58,40 @@ definition sem_step ::
    'mstate \<Rightarrow>
    ('mstate option)" where
 "sem_step gs m =
-  (case s_next gs m of
-    Exec sub \<Rightarrow> Some (s_sem gs sub m)
-    | f \<Rightarrow> None)"
+  (case s_cont gs m of
+    None \<Rightarrow> None
+    | Some [] \<Rightarrow> None
+    | Some ((G x l)#tt) \<Rightarrow> Some (s_sem gs x m))"
 
+(* Another option: leave an "open port" in all control flow modules?
+
+see if this will break any typeclasses...
+
+*)
 
 (* maybe use a relational small-step, relate it to the executable one? *)
-(* transitive closure? *)
 fun sem_exec ::
   "('x, 'mstate) sem \<Rightarrow>
    nat \<Rightarrow>
    'mstate \<Rightarrow>
-   ('mstate * 'x s_error)" where
-"sem_exec gs 0 m = (m, s_next gs m )"
+   ('mstate * s_error)" where
+"sem_exec gs 0 m = 
+  (case s_cont gs m of
+    Some [] \<Rightarrow> (m, Done)
+    | Some _ \<Rightarrow> (m, NoFuel)
+    | None \<Rightarrow> (m, Crash))"
 | "sem_exec gs (Suc n) m =
-   (case s_next gs m of
-    Done \<Rightarrow> (m, Halted)
-    | Exec sub \<Rightarrow> sem_exec gs n (s_sem gs sub m)
-    | f \<Rightarrow> (m, f))"
+   (case s_cont gs m of
+    Some [] \<Rightarrow> (m, Halted)
+    | Some ((G x l)#tt) \<Rightarrow> sem_exec gs n (s_sem gs x m)
+    | None \<Rightarrow> (m, Crash))"
 
 inductive sem_step_p ::
   "('x, 'mstate) sem \<Rightarrow> 'mstate \<Rightarrow> 'mstate \<Rightarrow> bool"
   where
-"\<And> (m :: 'mstate) (m' :: 'mstate) .
- s_next gs m = Exec sub \<Longrightarrow> 
- s_sem gs sub m = m' \<Longrightarrow>
+"\<And> (m :: 'mstate) (m' :: 'mstate) tt .
+ s_cont gs m = Some ((G x l)#tt) \<Longrightarrow> 
+ s_sem gs x m = m' \<Longrightarrow>
  sem_step_p gs m m'"
 
 definition sem_exec_p ::
@@ -90,18 +111,18 @@ qed
 lemma sem_step_sem_step_p :
   assumes H : "sem_step gs m = Some m'"
   shows "sem_step_p gs m m'" using H
-  by(auto simp add: sem_step_def split: s_error.splits intro: sem_step_p.intros)
+  by(auto simp add: sem_step_def split: s_error.splits list.splits option.splits intro: sem_step_p.intros)
 
 lemma sem_step_exec1 :
   assumes H : "sem_step gs m = Some m'"
   shows "\<exists> s . (sem_exec gs 1 m = (m', s))" using H
-  by(auto simp add: sem_step_def  split:s_error.splits)
+  by(auto simp add: sem_step_def  split:list.splits option.splits)
 
 lemma sem_exec1_step :
   assumes H : "(sem_exec gs 1 m = (m', s))"
   assumes H' : "s_error_safe s"
   shows "sem_step gs m = Some m'" using assms
-  by(auto simp add: sem_step_def split:s_error.splits)
+  by(auto simp add: sem_step_def split:option.splits list.splits)
 
 (*
 lemma sem_exec_snd :
@@ -117,27 +138,16 @@ next
 qed
 *)
 
-
+(* have the state contain a delta to the continuation list?
+   that is, a new prefix to prepend *)
 definition imm_safe :: "('x, 'mstate) sem \<Rightarrow> 'mstate \<Rightarrow> bool" where
 "imm_safe gs m \<equiv>
- ((s_next gs m = Done) \<or>
-  (\<exists> x . s_next gs m = Exec x \<and> s_error_safe (s_next gs (s_sem gs x m))))"
+ ((s_cont gs m = Some []) \<or>
+  (\<exists> m' . sem_step_p gs m m'))"
 
 definition safe :: "('x, 'mstate) sem \<Rightarrow> 'mstate \<Rightarrow> bool" where
 "safe gs m \<equiv>
   (\<forall> m' . sem_exec_p gs m m' \<longrightarrow> imm_safe gs m')"
-
-(* ugh.... still, would be great to have a way to separate state from
-  "next control".
-maybe we are ok? just add the qualification that s_next is the syntax of the command?
-*)
-
-(*
-definition safe :: "('x, 'mstate) sem \<Rightarrow> 'mstate \<Rightarrow> bool" where
-"safe gs m \<equiv>
- (imm_safe gs m \<or>
- ())
-*)
 
 (* classic question: want constraint on command below or above line? *)
 (* universally quantify over gs? *)
